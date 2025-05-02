fb0a60dad4944043b8fa8f5cff0cf45378c2759e
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-02 19:59:08 +0000

    fix: annotation and documentation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import bcrypt
+"""
+module defines hash_password function
+"""
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
+    """
+     expects one string argument name password and returns
+     a salted, hashed password, which is a byte string.
+    """
     password = b'password'
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     return hashed
