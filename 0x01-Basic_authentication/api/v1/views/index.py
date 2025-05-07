7c6af073bcd702d102775defe45795d7c5c2f38d
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-07 11:50:25 +0000

    fix: pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,11 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-  """
-  Raises:
-      401 error using abort
-  """
-  abort(401)
+    """
+    Raises:
+        401 error using abort
+    """
+    abort(401)
