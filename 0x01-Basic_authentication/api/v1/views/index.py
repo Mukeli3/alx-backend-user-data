7ccc2a87e4fee5413f8902292871e91d699da725
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-08 19:42:34 +0000

    feat: forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
         401 error using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    Raises:
+        403 error using abort
+    """
+    abort(403)
