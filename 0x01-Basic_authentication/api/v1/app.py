ac34fa2cd5e1a24753b362fd4a0a7a33b1c7dfe5
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-08 19:40:26 +0000

    feat: auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,15 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None  # initialize auth
+
+#  load authentication instance based on AUTH_TYPE
+auth_type = getenv('AUTH_TYPE')
+if auth_type:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -30,7 +39,33 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    403 error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_handler():
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 if __name__ == "__main__":
+    export AUTH_TYPE=basic_auth
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
