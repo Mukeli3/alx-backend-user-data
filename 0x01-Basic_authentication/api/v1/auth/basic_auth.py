8001209384b5ef66fa9bd3e2ad086c5257bd5d37
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-16 13:12:51 +0000

    feat: Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """
     Basic authentication class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """
+        returns the Authorization header Base64 part for a
+        Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ", 1)[1]
