c0196fb0eab3e48ab54d81896ad0e9617f9907de
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-16 19:36:39 +0000

    feat: decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic auth
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +23,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        returns the decoded value of a Base64 str
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header, validate=True)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
