ee2e885e28db2b01ef2d09842e9c3e5c27bfa0ff
Mukeli3 <mukelicynthia20@gmail.com>
2025-05-17 09:26:06 +0000

    feat: complete Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -74,3 +74,22 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        b64 = self.extract_base64_authorization_header(auth_header)
+        if b64 is None:
+            return None
+        decoded = self.decode_base64_authorization_header(b64)
+        if decoded is None:
+            return None
+        email, password = self.extract_user_credentials(decoded)
+        if email is None or password is None:
+            return None
+        user = self.user_object_from_credentials(email, password)
+        return user
