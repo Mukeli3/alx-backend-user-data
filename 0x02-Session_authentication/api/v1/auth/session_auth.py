98f4b6484af7e874de3eb3b4847d564a737f483a
Mukeli3 <mukelicynthia20@gmail.com>
2025-06-19 14:28:51 +0000

    retrieve user id on a session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,11 +2,24 @@
 """
 session authentication
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """
-    empty class
+    Session Authentication
     """
-    pass
+    user_id_by_session_id = {}  # class attribute
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a session id for a user id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        # map session ID to user ID
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
