dd927b64f8dc5dc13f96715020d0c1b7af123b14
Mukeli3 <mukelicynthia20@gmail.com>
2025-06-19 17:57:49 +0000

    fix: execution

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,8 +51,8 @@ def before_request_handler():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None
+    if auth.authorization_header(request) is None and
+    auth.session_cookie(request) is None:
         abort(401)
 
 
