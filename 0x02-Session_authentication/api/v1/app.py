c2832ee0a74ff0b2052530a18df2bcddf1681667
Mukeli3 <mukelicynthia20@gmail.com>
2025-06-19 14:00:39 +0000

    session_auth variable

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,10 @@ elif auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
 EXCLUDED_PATHS = ['/api/v1/status/', '/api/v1/unauthorized/',
                   '/api/v1/forbidden/']
 
